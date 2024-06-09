cdf1e7955f03663211cd2f6d93f4a3cfa6269d0b
cat3699 <catarina.m.branco@tecnico.ulisboa.pt>
2024-06-09 19:57:43 +0100

    try nr 1000

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class Multiplication {
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static void main(String[] args) {
+        int result = multiply(5, 10);
+        System.out.println("Multiplication result: " + result);
+    }
+}
+
